161c0273c0d0de0f44bfe5179743ae45e375bd80
hanna <hannawkozak@gmail.com>
2025-10-11 00:05:32 +0100

    feat: add vercel analitics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Hero } from './components/Hero/Hero';
 import { Layout } from './components/Layout/Layout';
 import { Portfolio } from './components/Portfolio/Portfolio';
+import { Analytics } from '@vercel/analytics/next';
 
 const App = () => {
   return (
     <Layout>
       <Hero />
       <Portfolio />
+      <Analytics />
     </Layout>
   );
 };
