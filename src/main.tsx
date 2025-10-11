4cea951a0a8e3b0993854d6529dfbe88d28988ed
hanna <hannawkozak@gmail.com>
2025-10-11 22:17:32 +0100

    fix: fix vercel analitics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/index.css';
+import { inject } from '@vercel/analytics';
+
+inject();
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
