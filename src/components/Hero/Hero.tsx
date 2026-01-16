b5530839df7cbe4594188b893136359e6143dc32
hanna <hannawkozak@gmail.com>
2026-01-16 12:01:05 +0000

    change content

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,13 +5,12 @@ export const Hero = () => {
     <div className="container prose max-w-none flex flex-col h-screen justify-center items-start">
       <p className="text-cyan-300 m-0 tracking-wider">Hi, my name is</p>
       <h2 className={`${styles.textGradient} text-6xl`}>Hanna Kozak</h2>
-      <h1 className={`${styles.textGradient} text-6xl invisible md:visible`}>
-        Frontend Developer
-      </h1>
-      <p className="tracking-wider py-4 text-white w-100 md:w-3/4 xl:w-1/2 -mt-24 md:mt-0">
-        I&apos;am a frontend developer focused on creating clean & user-firendly
-        websites and application. Currently, I&apos;m actively searching for my
-        next challenge in the frontend development space.
+      <h1 className={`${styles.textGradient} text-6xl`}>Web Developer</h1>
+      <p className="tracking-wider py-4 text-white w-80 md:w-3/4 xl:w-1/2 -mt-24 md:mt-0">
+        I'm a web developer passionate about building clean, user friendly
+        websites and applications. I enjoy working across the frontend and
+        fullstack spectrum and am currently seeking my next challenge in web
+        development.
       </p>
       <a href="#portfolio">
         <button className="rounded border-2 border-cyan-300  text-cyan-300 tracking-wider py-2 px-4 hover:text-purple-500 hover:border-purple-500">
